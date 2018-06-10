0b9de4da3e6a25dcf682cf046482def0b2a81cd1
Victor Chuah <vchuah01@dcs.bbk.ac.uk>
2018-06-10 18:13:43 +0100

    VCBlockchain file

--- VCBlockchain.py
+++ VCBlockchain.py
@@ -7,7 +7,18 @@ Created on Sun Jun 10 17:39:56 2018
 Reference:
     https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248b
 """
-
 import hashlib as hasher
 
-    
+"""This blockchain stores a timestamp and an index. Each block will have
+a self-identifying hash. Each block's hash will be a cryptographic hash of the
+block's index, timestamp data, and the hash of the previous block's hash.
+The data can be anything you want"""
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp,
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+        
