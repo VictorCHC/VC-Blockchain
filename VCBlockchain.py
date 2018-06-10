54bb57339a8ddcd8cf2d70988c173d9338ecedb3
Victor Chuah <vchuah01@dcs.bbk.ac.uk>
2018-06-10 17:52:04 +0100

    VCBlockchain file

--- VCBlockchain.py
+++ VCBlockchain.py
@@ -19,3 +19,10 @@ def hashMe(msg=""):
     if type(msg)!=str:
         msg = json.dumps(msg,sort_keys=True)
         # If keys are unsorted we can't guarantee repeatability
+    
+    if sys.version_info.major == 2:
+        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
+    else:
+        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
+    
+    
