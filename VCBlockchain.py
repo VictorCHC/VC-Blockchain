c7d58baef4535a2ae1050240444cf220b1fdae61
Victor Chuah <vchuah01@dcs.bbk.ac.uk>
2018-06-10 18:08:57 +0100

    VCBlockchain file

--- VCBlockchain.py
+++ VCBlockchain.py
@@ -3,26 +3,11 @@
 Created on Sun Jun 10 17:39:56 2018
 
 @author: VictorCHC
+
+Reference:
+    https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248b
 """
 
-"""Reference:
-    http://ecomunsing.com/build-your-own-blockchain"""
+import hashlib as hasher
 
-import hashlib, json, sys
-
-"""A hash function is created as a ‘fingerprint’ for each transactions- 
-this hash function links each of our blocks to each other"""
-
-def hashMe(msg=""):
-    """Helper function that wraps our hashing algorithm"""
-    
-    if type(msg)!=str:
-        msg = json.dumps(msg,sort_keys=True)
-        # If keys are unsorted we can't guarantee repeatability
-    
-    if sys.version_info.major == 2:
-        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
-    else:
-        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
-    
     
