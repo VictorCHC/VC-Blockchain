bd8be79b2604dcefd2c415fd7ae80b04ac833fce
Victor Chuah <vchuah01@dcs.bbk.ac.uk>
2018-06-10 18:50:55 +0100

    VCBlockchain file

--- VCBlockchain.py
+++ VCBlockchain.py
@@ -84,5 +84,5 @@ for i in range(num_of_blocks_to_add):
     blockchain.append(block_to_add)
     previous_block = block_to_add
     
-    print ("Block #{} has been added to the blockchain!").format(block_to_add.index)
-    print ("Hash: {}\n").format(block_to_add.hash)
+    print ("Block #{} has been added to the blockchain!".format(block_to_add.index))
+    print ("Hash: {}\n".format(block_to_add.hash))
